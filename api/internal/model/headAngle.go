ab9c5b94ca516000d9ac9b707404fd967d20a779
bipblop555 <tbonnet@meylly.com>
2024-05-26 01:18:59 +0200

    headAngle set

---
+++ api/internal/model/headAngle.go
@@ -0,0 +1,3 @@
+package model
+
+type HeadAngle [2]int
--- api/internal/service/services.go
+++ api/internal/service/services.go
@@ -6,6 +6,14 @@ type ControlService struct {
 	controlRepo facade.ControlRepository
 }
 
+type HeadAngleService struct {
+	headAngleRepo facade.HeadAngleRepository
+}
+
 func NewControlService(controlRepo facade.ControlRepository) *ControlService {
 	return &ControlService{controlRepo}
+}
+
+func NewHeadAngleService(headAngleRepo facade.HeadAngleRepository) *HeadAngleService {
+	return &HeadAngleService{headAngleRepo}
 }
