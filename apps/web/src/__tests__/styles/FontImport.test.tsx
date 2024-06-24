178a6c77be080377495d34d98d8caa2445799957
theVicoH <huang.victor.mail@gmail.com>
2024-06-24 21:57:01 +0200

    Fix text font import test

--- apps/web/src/__tests__/styles/FontImport.test.tsx
+++ apps/web/src/__tests__/styles/FontImport.test.tsx
@@ -2,7 +2,7 @@ import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import React from "react";
 
-const FontTester: React.FC = () => {
+const FontImport: React.FC = () => {
   return (
     <div>
       <div className="font-dsDigital" style={{ fontFamily: "DSDigitalBold" }}>DS-Digital Bold</div>
@@ -11,7 +11,7 @@ const FontTester: React.FC = () => {
   );
 }
 
-describe("FontTester component", () => {
+describe("Font imports", () => {
   const fonts = [
     { text: "DS-Digital Bold", fontClass: "font-dsDigital", fontFamily: "DSDigitalBold" },
     { text: "NotoSans", fontClass: "font-notoSans", fontFamily: "NotoSans" },
@@ -19,13 +19,13 @@ describe("FontTester component", () => {
 
   fonts.forEach(({ text, fontClass, fontFamily }) => {
     it(`should have the class '${fontClass}' applied to the text '${text}'`, () => {
-      const { getByText } = render(<FontTester />);
+      const { getByText } = render(<FontImport />);
       const element = getByText(text);
       expect(element).toHaveClass(fontClass);
     });
 
     it(`should apply the font-family '${fontFamily}' to the text '${text}'`, () => {
-      const { getByText } = render(<FontTester />);
+      const { getByText } = render(<FontImport />);
       const element = getByText(text);
       expect(window.getComputedStyle(element).fontFamily).toBe(fontFamily);
     });
