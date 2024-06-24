d898ca21c0d5ef283f8c7e0c2ebc4ea66e495552
theVicoH <huang.victor.mail@gmail.com>
2024-06-24 21:52:49 +0200

    Update vite web

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import path from "path"
+import { configDefaults } from "vitest/config"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -11,4 +11,10 @@ export default defineConfig({
       "@common": path.resolve(__dirname, "../../packages/common/src"),
     },
   },
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    include: [...configDefaults.include, "**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+  },
+});
