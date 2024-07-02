e8e701eef34515f5a6829a111936baeb17210232
bipblop555 <tbonnet@meylly.com>
2024-07-02 14:45:29 +0200

    update(cd) model WheelSpeed et WheelRawData

--- apps/api/internal/model/control.go
+++ apps/api/internal/model/control.go
@@ -1,11 +1,11 @@
 package model
 
-type WheelSpeed [4]int
+type WheelSpeed [4]float32
 
 type WheelRawData struct {
 	X     float32 `json:"x"`
 	Y     float32 `json:"y"`
-	Force float32 `json:"float"`
+	Force float32 `json:"force"`
 }
 
 type ConfWheelSpeed struct {
