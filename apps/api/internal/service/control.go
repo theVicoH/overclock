009208af3a8e6ec3e474adfb307c0ed3ee1f16f8
bipblop555 <tbonnet@meylly.com>
2024-07-02 14:45:49 +0200

    update(cd) translate data raw en data esp

--- apps/api/internal/service/control.go
+++ apps/api/internal/service/control.go
@@ -5,15 +5,6 @@ import (
 	"fmt"
 )
 
-func (s *ControlService) IsValidSpeed(speeds model.WheelSpeed) bool {
-	for _, value := range speeds {
-		if value < -4095 || value > 4095 {
-			return false
-		}
-	}
-	return true
-}
-
 func (s *ControlService) Direction(speeds model.WheelSpeed) error {
 
 	err := s.controlRepo.Direction(speeds)
@@ -22,4 +13,40 @@ func (s *ControlService) Direction(speeds model.WheelSpeed) error {
 	}
 
 	return nil
+}
+
+func (s *ControlService) TransformRawData(wheelRawData model.WheelRawData) model.WheelSpeed {
+	// X > 0 -> droite
+	// X < 0 -> gauche
+	// Y > 0 -> bas
+	// Y < 0 -> haut
+	var wheel model.WheelSpeed
+	// on multiplie par la force et on envoie [4500 x force, 4500 x force, 4500 x force, 4500 x force]
+
+	if (wheelRawData.X >= -25 && wheelRawData.X <= 25) && wheelRawData.Y < 0 {
+		fmt.Println(wheelRawData.X)
+		wheel = model.WheelSpeed{4095 * wheelRawData.Force, 4095 * wheelRawData.Force, 4095 * wheelRawData.Force, 4095 * wheelRawData.Force}
+
+		fmt.Println(wheel)
+	} else if (wheelRawData.X >= -25 && wheelRawData.X <= 25) && wheelRawData.Y > 0 {
+		fmt.Println(wheelRawData.X)
+		wheel = model.WheelSpeed{-4095 * wheelRawData.Force, -4095 * wheelRawData.Force, -4095 * wheelRawData.Force, -4095 * wheelRawData.Force}
+
+		fmt.Println(wheel)
+	} else if wheelRawData.X > 25 {
+		wheel = model.WheelSpeed{4095 * wheelRawData.Force, 4095 * wheelRawData.Force, 0, 0}
+
+		fmt.Println(wheel)
+
+	} else if wheelRawData.X < 25 {
+		wheel = model.WheelSpeed{0, 0, 4095 * wheelRawData.Force, 4095 * wheelRawData.Force}
+
+		fmt.Println(wheel)
+	} else if wheelRawData.X == 0 && wheelRawData.Y == 0 && wheelRawData.Force == 0 {
+		wheel = model.WheelSpeed{0, 0, 0, 0}
+
+		fmt.Println(wheel)
+	}
+
+	return wheel
 }
