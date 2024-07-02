3da9d2b5b6402d9f8072744451189147d2e8276c
bipblop555 <tbonnet@meylly.com>
2024-07-02 11:16:11 +0200

    update(cd) new type rawData

--- apps/api/internal/model/control.go
+++ apps/api/internal/model/control.go
@@ -2,6 +2,12 @@ package model
 
 type WheelSpeed [4]int
 
+type WheelRawData struct {
+	X     float32 `json:"x"`
+	Y     float32 `json:"y"`
+	Force float32 `json:"float"`
+}
+
 type ConfWheelSpeed struct {
 	Cmd        `json:"cmd"`
 	WheelSpeed `json:"data"`
