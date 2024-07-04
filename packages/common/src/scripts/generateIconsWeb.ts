2d3de2cb7021ca2f8ebd9dc724d364015f8e4096
theVicoH <victor.huang@shadow.tech>
2024-07-04 17:28:55 +0200

    Fix generateIconsWeb import

--- packages/common/src/scripts/generateIconsWeb.ts
+++ packages/common/src/scripts/generateIconsWeb.ts
@@ -1,5 +1,5 @@
-const { execSync } = require("child_process")
-const path = require("path")
+import { execSync } from "child_process"
+import path from "path"
 
 const outDir: string = path.resolve(__dirname, "../icons/web")
 const srcDir: string = path.resolve(__dirname, "../assets/icons")
