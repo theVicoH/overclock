ef4c80624653ec137ab36ffe99fa9cd531eb1d45
theVicoH <victor.huang@shadow.tech>
2024-07-04 16:47:37 +0200

    Transform generateIconsWeb.js to ts

--- packages/common/scripts/generateIconsWeb.ts
+++ packages/common/scripts/generateIconsWeb.ts
@@ -1,7 +1,7 @@
 const { execSync } = require("child_process")
 const path = require("path")
 
-const outDir = path.resolve(__dirname, "../src/icons/web")
-const srcDir = path.resolve(__dirname, "../src/assets/icons")
+const outDir: string = path.resolve(__dirname, "../src/icons/web")
+const srcDir: string = path.resolve(__dirname, "../src/assets/icons")
 
 execSync(`cross-env svgr --typescript --out-dir ${outDir} ${srcDir}`, { stdio: "inherit" })
