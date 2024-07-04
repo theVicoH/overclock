98805018288da7f97d80d422070d91e6370a84d8
theVicoH <victor.huang@shadow.tech>
2024-07-04 16:49:28 +0200

    Run prettier

--- packages/common/scripts/generateIconsMobile.ts
+++ packages/common/scripts/generateIconsMobile.ts
@@ -1,7 +1,7 @@
-import { execSync } from "child_process";
-import path from "path";
+import { execSync } from "child_process"
+import path from "path"
 
-const outDir: string = path.resolve(__dirname, "../src/icons/mobile");
-const srcDir: string = path.resolve(__dirname, "../src/assets/icons");
+const outDir: string = path.resolve(__dirname, "../src/icons/mobile")
+const srcDir: string = path.resolve(__dirname, "../src/assets/icons")
 
-execSync(`cross-env svgr --typescript --native --out-dir ${outDir} ${srcDir}`, { stdio: "inherit" });
+execSync(`cross-env svgr --typescript --native --out-dir ${outDir} ${srcDir}`, { stdio: "inherit" })
