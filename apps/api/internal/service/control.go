304876d9933ff9595f41e6582420c690f55d1bee
bipblop555 <tbonnet@meylly.com>
2024-07-08 11:08:37 +0200

    update(cd) gestion si valeur egale on envoie pas

--- apps/api/internal/service/control.go
+++ apps/api/internal/service/control.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 )
 
-var wheel model.WheelSpeed
+var (
+	wheel     model.WheelSpeed
+	prevValue model.WheelRawData
+)
 
 func (s *ControlService) Direction(speeds model.WheelSpeed) error {
 
@@ -17,36 +20,47 @@ func (s *ControlService) Direction(speeds model.WheelSpeed) error {
 	return nil
 }
 
-func (s *ControlService) TransformRawData(wheelRawData model.WheelRawData) model.WheelSpeed {
+func (s *ControlService) TransformRawData(wheelRawData model.WheelRawData) (model.WheelSpeed, bool) {
 	// X > 0 -> droite
 	// X < 0 -> gauche
 	// Y > 0 -> bas
 	// Y < 0 -> haut
 	// on multiplie par la force et on envoie [4500 x force, 4500 x force, 4500 x force, 4500 x force]
+	fmt.Println(prevValue, "&&", wheelRawData)
 
-	if (wheelRawData.X >= -25 && wheelRawData.X <= 25) && wheelRawData.Y < 0 {
-		wheel = s.GoForward(wheelRawData)
+	if prevValue != wheelRawData {
+		prevValue = wheelRawData
 
-	} else if (wheelRawData.X >= -25 && wheelRawData.X <= 25) && wheelRawData.Y > 0 {
-		wheel = s.GoBack(wheelRawData)
+		if wheelRawData.X == 0 && wheelRawData.Y == 0 && wheelRawData.Force == 0 {
+			wheel = s.Stop(wheelRawData)
 
-	} else if wheelRawData.X > 25 {
-		wheel = s.TurnLeft(wheelRawData)
+		} else if (wheelRawData.X >= -25 && wheelRawData.X <= 25) && wheelRawData.Y < 0 {
+			wheel = s.GoForward(wheelRawData)
 
-	} else if wheelRawData.X < 25 {
-		wheel = s.TurnRight(wheelRawData)
+		} else if (wheelRawData.X >= -25 && wheelRawData.X <= 25) && wheelRawData.Y > 0 {
+			wheel = s.GoBack(wheelRawData)
 
-	} else if wheelRawData.X == 0 && wheelRawData.Y == 0 && wheelRawData.Force == 0 {
-		wheel = s.Stop(wheelRawData)
+		} else if wheelRawData.X > 25 {
+			wheel = s.TurnLeft(wheelRawData)
+
+		} else if wheelRawData.X < 25 {
+			wheel = s.TurnRight(wheelRawData)
+
+		}
+
+	} else {
+
+		fmt.Println("wheel else", wheel)
+		return wheel, false
 
 	}
 
-	return wheel
+	return wheel, true
 }
 
 func (s *ControlService) GoForward(wheelRawData model.WheelRawData) model.WheelSpeed {
 
-	wheel = model.WheelSpeed{4095 * wheelRawData.Force, 4095 * wheelRawData.Force, 4095 * wheelRawData.Force, 4095 * wheelRawData.Force}
+	wheel = model.WheelSpeed{2095, 2095, 2095, 2095}
 	fmt.Println("GO FORWARD", wheel)
 
 	return wheel
@@ -54,7 +68,7 @@ func (s *ControlService) GoForward(wheelRawData model.WheelRawData) model.WheelS
 
 func (s *ControlService) GoBack(wheelRawData model.WheelRawData) model.WheelSpeed {
 
-	wheel = model.WheelSpeed{-4095 * wheelRawData.Force, -4095 * wheelRawData.Force, -4095 * wheelRawData.Force, -4095 * wheelRawData.Force}
+	wheel = model.WheelSpeed{-2095, -2095, -2095, -2095}
 
 	fmt.Println("GO BACK", wheel)
 	return wheel
@@ -63,18 +77,18 @@ func (s *ControlService) GoBack(wheelRawData model.WheelRawData) model.WheelSpee
 
 func (s *ControlService) TurnLeft(wheelRawData model.WheelRawData) model.WheelSpeed {
 
-	wheel = model.WheelSpeed{4095 * wheelRawData.Force, 4095 * wheelRawData.Force, -4095 * wheelRawData.Force, -4095 * wheelRawData.Force}
+	wheel = model.WheelSpeed{2095, 2095, -2095, -2095}
 
-	fmt.Println("TURN LEFT <-", wheel)
+	fmt.Println("TURN LEFT", wheel)
 	return wheel
 
 }
 
 func (s *ControlService) TurnRight(wheelRawData model.WheelRawData) model.WheelSpeed {
 
-	wheel = model.WheelSpeed{-4095 * wheelRawData.Force, -4095 * wheelRawData.Force, 4095 * wheelRawData.Force, 4095 * wheelRawData.Force}
+	wheel = model.WheelSpeed{-2095, -2095, 2095, 2095}
 
-	fmt.Println("TURN RIGHT ->", wheel)
+	fmt.Println("TURN RIGHT", wheel)
 	return wheel
 
 }
@@ -82,7 +96,7 @@ func (s *ControlService) Stop(wheelRawData model.WheelRawData) model.WheelSpeed 
 
 	wheel = model.WheelSpeed{0, 0, 0, 0}
 
-	fmt.Println("STOP || ", wheel)
+	fmt.Println("STOP", wheel)
 	return wheel
 
 }
