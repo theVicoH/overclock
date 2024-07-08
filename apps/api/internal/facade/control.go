a76dbcdeeeb477058f73fee05fe91b9811f36281
bipblop555 <tbonnet@meylly.com>
2024-07-08 16:03:13 +0200

    update(cd): time et prevValue

--- apps/api/internal/facade/control.go
+++ apps/api/internal/facade/control.go
@@ -1,9 +1,15 @@
 package facade
 
-import "Overclock/internal/model"
+import (
+	"Overclock/internal/model"
+	"time"
+)
 
 type ControlService interface {
 	TransformRawData(wheelRawData model.WheelRawData) (model.WheelSpeed, bool)
+
+	MoreThan10Ms(now time.Time) bool
+	ValueAreDifferent(prevValue model.WheelRawData, wheelRawData model.WheelRawData) bool
 
 	GoForward(wheelRawData model.WheelRawData) model.WheelSpeed
 	Stop(wheelRawData model.WheelRawData) model.WheelSpeed
