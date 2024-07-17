263038db7abdb9956eb38ac5d93e93640d828669
bipblop555 <tbonnet@meylly.com>
2024-07-17 21:30:43 +0200

    suppression ancien main.go

--- apps/api/cmd/main.go
+++ apps/api/cmd/main.go
@@ -1,68 +1,16 @@
 package main
 
 import (
-	"Overclock/internal/handler"
-	"Overclock/internal/repository"
-	"Overclock/internal/router"
-	"Overclock/internal/service"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
-	_ "Overclock/internal/doc"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/recover"
-	"github.com/gofiber/swagger"
-	"github.com/gorilla/websocket"
 )
-
-var (
-	controlHandler *handler.ControlHandler
-	buzzerHandler  *handler.BuzzerHandler
-	videoHandler   *handler.VideoVariableHandler
-	faceHandler    *handler.FaceHandler
-)
-
-func init() {
-	// db := database.ConnectDB(database.DBConfig{
-	// 	Host:     os.Getenv("DB_HOST"),
-	// 	Port:     os.Getenv("DB_PORT"),
-	// 	User:     os.Getenv("DB_USER"),
-	// 	Password: os.Getenv("DB_PASSWORD"),
-	// 	Name:     os.Getenv("DB_NAME"),
-	// 	Type:     os.Getenv("DB_TYPE"),
-	// })
-
-	// u := url.URL{Scheme: "ws", Host: os.Getenv("WS_IP"), Path: "/overclock"}
-
-	// conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-	conn, _, err := websocket.DefaultDialer.Dial("ws://192.168.83.90/overclock", nil)
-
-	if err != nil {
-		log.Fatalf("Error establishing WebSocket connection: %v", err)
-	}
-	// defer conn.Close()
-
-	controlRepo := repository.NewControlRepository(conn)
-	buzzerRepo := repository.NewBuzzerRepository(conn)
-	faceRepo := repository.NewFaceRepository(conn)
-	videoRepo := repository.NewVideoRepository(conn)
-
-	controlService := service.NewControlService(controlRepo)
-	buzzerService := service.NewBuzzerService(buzzerRepo)
-	faceService := service.NewFaceService(faceRepo)
-	videoService := service.NewVideoService(videoRepo)
-
-	controlHandler = handler.NewControlHandler(controlService)
-	faceHandler = handler.NewFaceHandler(faceService)
-	buzzerHandler = handler.NewBuzzerHandler(buzzerService)
-	videoHandler = handler.NewVideoHandler(videoService)
-
-}
 
 func main() {
 	app := fiber.New()
@@ -72,12 +20,6 @@ func main() {
 		AllowMethods: "GET, POST, PUT, DELETE",
 	}))
 	app.Use(recover.New())
-
-	app.Get("/swagger/*", swagger.HandlerDefault)
-	router.ControlRoutes(app, controlHandler)
-	router.BuzzerRoutes(app, buzzerHandler)
-	router.VideoRoutes(app, videoHandler)
-	router.FaceRoutes(app, faceHandler)
 
 	app.Use(func(c *fiber.Ctx) error {
 		return c.SendStatus(fiber.StatusNotFound)
