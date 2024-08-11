549e7eb63a95f9c029b0c1293be105a8f35f766b
jugurtamahious <jugurtamahious@gmail.com>
2024-08-11 21:20:09 +0200

    change(type): changes statsRace Type

--- apps/api/internal/types/stats_race_type.go
+++ apps/api/internal/types/stats_race_type.go
@@ -7,6 +7,7 @@ type StatsRaceType struct {
 	RaceId    string    `json:"race_id"`
 	VehicleId string    `json:"vehicle_id"`
 	Distance  float32   `json:"distance"`
+	Speed     float32   `json:"speed"`
 	Time      time.Time `json:"time"`
 	DateTech  time.Time `json:"date_tech"`
 }
