b8b96c60be1429d12585ad4b36924b006de39b70
jugurtamahious <jugurtamahious@gmail.com>
2024-08-16 15:18:03 +0200

    update(db): upadte sensor and stat_race tables

--- docker/init.sql
+++ docker/init.sql
@@ -10,6 +10,7 @@ CREATE TABLE "vehicle" (
 
 CREATE TABLE "race" (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    vehicle_id UUID NOT NULL,
     name VARCHAR(255) NOT NULL,
     date TIMESTAMP NOT NULL
 );
@@ -19,6 +20,7 @@ CREATE TABLE "stats_race" (
     race_id UUID NOT NULL,
     vehicle_id UUID NOT NULL,
     distance FLOAT NOT NULL,
+    speed FLOAT NOT NULL,
     time TIMESTAMP NOT NULL,
     date_tech TIMESTAMP NOT NULL,
     CONSTRAINT fk_race_stats_race FOREIGN KEY (race_id) REFERENCES race(id) ON DELETE CASCADE,
@@ -28,11 +30,8 @@ CREATE TABLE "stats_race" (
 CREATE TABLE "sensor_data" (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     race_id UUID NOT NULL,
-    vehicle_id UUID NOT NULL,
+    distance FLOAT NOT NULL,
     speed FLOAT NOT NULL,
-    orientation JSONB NOT NULL,
     date_tech TIMESTAMP NOT NULL,
-    consumption FLOAT NOT NULL,
-    CONSTRAINT fk_race_sensor_data FOREIGN KEY (race_id) REFERENCES race(id) ON DELETE CASCADE,
-    CONSTRAINT fk_sensor_data_vehicle FOREIGN KEY (vehicle_id) REFERENCES vehicle(id) ON DELETE CASCADE
+    consumption FLOAT NOT NULL
 );
