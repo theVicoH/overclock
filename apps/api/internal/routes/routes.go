71579eb40addfb47e723c416a2a503186ac9ae43
bipblop555 <tbonnet@meylly.com>
2024-08-19 10:44:11 +0200

    update(cd): ajout routes vehicle

--- apps/api/internal/routes/routes.go
+++ apps/api/internal/routes/routes.go
@@ -19,8 +19,14 @@ func SetRoute(app *fiber.App, handler *handler.HandlerStruct) {
 
 	statsRaceGroup := app.Group("/stats_race")
 	statsRaceGroup.Get("/:id", handler.GetStatsRaceById)
-	// statsRaceGroup.Get("/", handler.GetAllRace)
 	statsRaceGroup.Get("/", handler.AddStatsRace)
 	statsRaceGroup.Delete("/:id", handler.DeleteStatsRaceById)
 	statsRaceGroup.Put("/:id", handler.UpdateStatsRaceById)
+
+	vehicleGroup := app.Group("/vehicle")
+	vehicleGroup.Get("/:id", handler.GetVehicleById)
+	vehicleGroup.Get("/", handler.GetAllVehicle)
+	vehicleGroup.Post("/", handler.AddVehicle)
+	vehicleGroup.Delete("/:id", handler.DeleteVehicleById)
+	vehicleGroup.Put("/:id", handler.UpdateVehicleById)
 }
