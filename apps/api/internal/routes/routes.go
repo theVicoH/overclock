38f5bae5b8bf552271c293d06d80d2fc859c0814
bipblop555 <tbonnet@meylly.com>
2024-09-02 14:43:18 +0200

    update(cd): modif routes supprimées

--- apps/api/internal/routes/routes.go
+++ apps/api/internal/routes/routes.go
@@ -7,23 +7,15 @@ import (
 )
 
 func SetRoute(app *fiber.App, handler *handler.HandlerStruct) {
-	sensorGroup := app.Group("/sensor")
-	sensorGroup.Get("/:id", handler.GetSensorDataById)
-	sensorGroup.Get("/speed/:id", handler.GetSpeedLastTenMin)
-	sensorGroup.Get("/consumption/:id", handler.GetConsumptionLastTenMin)
 
 	raceGroup := app.Group("/race")
 	raceGroup.Get("/:id", handler.GetRaceDetailsById)
 	raceGroup.Get("/", handler.GetAllRacesWithData)
 	raceGroup.Post("/", handler.AddRace)
 	raceGroup.Delete("/:id", handler.DeleteRaceById)
-	raceGroup.Put("/:id", handler.UpdateRaceById)
 
 	statsRaceGroup := app.Group("/stats_race")
-	statsRaceGroup.Get("/:id", handler.GetStatsRaceById)
 	statsRaceGroup.Get("/", handler.AddStatsRace)
-	statsRaceGroup.Delete("/:id", handler.DeleteStatsRaceById)
-	statsRaceGroup.Put("/:id", handler.UpdateStatsRaceById)
 
 	vehicleGroup := app.Group("/vehicle")
 	vehicleGroup.Get("/:id", handler.GetVehicleById)
@@ -33,8 +25,5 @@ func SetRoute(app *fiber.App, handler *handler.HandlerStruct) {
 	vehicleGroup.Put("/:id", handler.UpdateVehicleById)
 
 	statsRaceGroup.Post("/:id", handler.AddStatsRace)
-	statsRaceGroup.Delete("/:id", handler.DeleteStatsRaceById)
-	// statsRaceGroup.Get("/", handler.GetAllRace)
-	// statsRaceGroup.Get("/", handler.AddStatsRace)
-	// statsRaceGroup.Put("/:id", handler.UpdateStatsRaceById)
+
 }
