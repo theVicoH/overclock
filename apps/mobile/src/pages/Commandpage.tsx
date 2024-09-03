b5969b73af1686384b3a0167d99f36a0fd103e54
vitolinho <93949338+vitolinho@users.noreply.github.com>
2024-09-03 10:00:10 +0200

    Remove any types in mobile codebase

--- apps/mobile/src/pages/Commandpage.tsx
+++ apps/mobile/src/pages/Commandpage.tsx
@@ -21,8 +21,9 @@ const Commandpage = ({ navigation }: CommandPageProps) => {
     socket.onopen = () => {
       console.log("WebSocket connection established.")
     }
+    // TODO se connecter à la voiture et log le typeof(data) pour savoir quoi mettre à la place de any
     socket.onmessage = (data: MessageEvent<any>) => {
-      console.log("Message from server:", data)
+      console.log("Message from server:", typeof(data))
     }
     socket.onerror = (error: Event) => {
       console.error("WebSocket error:", error)
