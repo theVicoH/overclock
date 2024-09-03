ea5633bc0d3785dd29681aa593f5f8b4370a6b86
vitolinho <93949338+vitolinho@users.noreply.github.com>
2024-09-03 10:00:10 +0200

    Remove any types in mobile codebase

--- apps/mobile/src/types/navigationProperties.ts
+++ apps/mobile/src/types/navigationProperties.ts
@@ -1,6 +1,7 @@
 import { StackNavigationProp } from "@react-navigation/stack"
 import { RouteProp } from "@react-navigation/native";
 import { RootStackParamList } from "./rootStackParamList"
+import { NavigationProp } from "@react-navigation/native"
 
 export type ManualPageNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -42,4 +43,8 @@ export type CommandPageProps = {
 export type AutoPageConnectProps = {
   navigation: ManualPageNavigationProp
   route: AutoPageConnectRouteProp
+}
+
+export type HeaderProps = {
+  navigation: NavigationProp<RootStackParamList>
 }
