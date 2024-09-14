d2abea2beb5cd085d471db2b6d2a26e6d733756a
jugurtamahious <jugurtamahious@gmail.com>
2024-09-14 20:48:04 +0200

    feat(vehicle routes): restore deleted vehicle routes

---
+++ apps/api/internal/handler/vehicle_handler.go
@@ -0,0 +1,169 @@
+package handler
+
+import (
+	"Overclock/internal/store"
+	"Overclock/internal/types"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/google/uuid"
+)
+
+func NewVehicleHandler(store *store.StoreStruct) *Handler {
+	return &Handler{
+		store,
+	}
+}
+
+func (h *Handler) AddVehicle(c fiber.Ctx) error {
+
+	var requestData types.RequestTypeVehicle
+
+	if err := c.Bind().Body(&requestData); err != nil {
+		return err
+	}
+
+	vehicleData := types.VehicleType{
+		Name: requestData.Data.Name,
+	}
+
+	vehicle, err := h.store.AddVehicle(vehicleData)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Error adding Vehicle",
+		})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"message": "Vehicle successfully added",
+		"data":    vehicle,
+	})
+
+}
+
+func (h *Handler) GetVehicleById(c fiber.Ctx) error {
+
+	id := c.Params("id")
+	vehicle, err := h.store.GetVehicleById(id)
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Error fetching vehicle",
+		})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"message": "Vehicle successfully added",
+		"data":    vehicle,
+	})
+
+}
+
+func (h *Handler) GetAllVehicle(c fiber.Ctx) error {
+	vehicle, err := h.store.GetAllVehicle()
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": vehicle,
+		})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"message": "Vehicle successfully fetched",
+		"data":    vehicle,
+	})
+}
+
+func (h *Handler) GetAllVehiclesWithRaces(c fiber.Ctx) error {
+	vehicules, err := h.store.GetAllVehiclesWithRaces()
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Error fetching vehicule",
+			"error":   err,
+		})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"message": "vehicule successfully fetched",
+		"date":    vehicules,
+	})
+}
+
+func (h *Handler) GetVehicleWithRacesById(c fiber.Ctx) error {
+	idStr := c.Params("id")
+	id, err := uuid.Parse(idStr)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid UUID format",
+		})
+	}
+	vehicule, err := h.store.GetVehicleWithRacesById(id)
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Error fetching vehicule",
+			"error":   err,
+		})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"message": "vehicule successfully fetched",
+		"date":    vehicule,
+	})
+}
+
+func (h *Handler) GetVehiclesStats(c fiber.Ctx) error {
+	vehicules, err := h.store.GetVehiclesStats()
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Error fetching vehicule",
+			"error":   err,
+		})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"message": "vehicule successfully fetched",
+		"date":    vehicules,
+	})
+}
+
+func (h *Handler) GetVehicleStatsById(c fiber.Ctx) error {
+	idStr := c.Params("id")
+	id, err := uuid.Parse(idStr)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid UUID format",
+		})
+	}
+	vehicules, err := h.store.GetVehicleStatsById(id)
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Error fetching vehicule",
+			"error":   err,
+		})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"message": "vehicule successfully fetched",
+		"date":    vehicules,
+	})
+}
+
+func (h *Handler) GetClassementBySpeed(c fiber.Ctx) error {
+	vehiculesclassementBySpeed, err := h.store.GetClassementBySpeed()
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Error fetching vehicule",
+			"error":   err,
+		})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"message": "vehicule successfully fetched",
+		"date":    vehiculesclassementBySpeed,
+	})
+}
--- apps/api/internal/types/vehicle_type.go
+++ apps/api/internal/types/vehicle_type.go
@@ -1,6 +1,8 @@
 package types
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 )
 
@@ -9,6 +11,52 @@ type VehicleType struct {
 	Name string    `json:"name"`
 }
 
+type RequestTypeVehicle struct {
+	Data VehicleUpdateType
+}
+
+type VehicleUpdateType struct {
+	Name string `json:"name"`
+}
+
 func (VehicleType) TableName() string {
 	return "vehicle"
+}
+
+type VehicleByRacesDetailType struct {
+	Id           uuid.UUID `json:"id"`
+	Name         string    `json:"name"`
+	Date         time.Time `json:"date"`
+	Time         int64     `json:"time,omitempty"`
+	SpeedAverage float64   `json:"speed_average,omitempty"`
+	Distance     float64   `json:"distance,omitempty"`
+}
+
+type VehicleWithRacesDetailType struct {
+	ID    string           `json:"id"`
+	Name  string           `json:"name"`
+	Races []RaceDetailType `json:"races" gorm:"-"`
+}
+
+type RaceDetailType struct {
+	ID           string    `json:"id"`
+	Name         string    `json:"name"`
+	Date         time.Time `json:"date"`
+	Time         int       `json:"time"`
+	SpeedAverage float64   `json:"speed_average"`
+	Distance     float64   `json:"distance"`
+}
+
+type VehicleStatsDetailType struct {
+	ID          string  `json:"id"`
+	Name        string  `json:"name"`
+	MaxSpeed    float64 `json:"max_speed"`
+	MaxDistance float64 `json:"max_distance"`
+	MinTime     int     `json:"min_time"`
+}
+
+type VehiculeClassementBySpeedMax struct {
+	ID       string  `json:"id"`
+	Name     string  `json:"name"`
+	MaxSpeed float32 `json:"max_speed"`
 }
