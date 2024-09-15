68097a5617b60d8b8578c517a678620c7c2d84c9
vitolinho <93949338+vitolinho@users.noreply.github.com>
2024-09-15 23:49:31 +0200

    Fix battery

--- apps/mobile/src/__tests__/components/Battery.test.tsx
+++ apps/mobile/src/__tests__/components/Battery.test.tsx
@@ -9,20 +9,21 @@ describe("Battery Component", () => {
     render(<BatteryComponent battery={75} />);
     const batteryFill = screen.getByTestId("batteryFill");
     expect(batteryFill.style.backgroundColor).toBe(hexToRgb(colors.white));
-    expect(batteryFill.style.width).toBe(`${(75 * 23) / 100}px`);
+    expect(batteryFill.style.width).toBe(`${Math.round(Math.floor((75 * 23) / 100))}px`);
   });
 
   test("displays correct color and size for medium battery", () => {
     render(<BatteryComponent battery={35} />);
     const batteryFill = screen.getByTestId("batteryFill");
     expect(batteryFill.style.backgroundColor).toBe(hexToRgb(colors.primary500));
-    expect(batteryFill.style.width).toBe(`${(35 * 23) / 100}px`);
+    expect(batteryFill.style.width).toBe(`${Math.round(Math.floor((35 * 23) / 100))}px`);
+
   });
 
   test("displays correct color and size for low battery", () => {
     render(<BatteryComponent battery={15} />);
     const batteryFill = screen.getByTestId("batteryFill");
     expect(batteryFill.style.backgroundColor).toBe(hexToRgb(colors.red500));
-    expect(batteryFill.style.width).toBe(`${(15 * 23) / 100}px`);
+    expect(batteryFill.style.width).toBe(`${Math.round(Math.floor((15 * 23) / 100))}px`);
   });
 });
